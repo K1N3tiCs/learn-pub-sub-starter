fe78747fccce1adbaea77504fedaa6b92f714eea
Trash Can <kinetic.0212@gmail.com>
2026-09-24 02:20:16 +0530

    connection to rabbitmq server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const brokerURI = "amqp://guest:guest@localhost:5672/"
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	conn, err := amqp.Dial(brokerURI)
+	if err != nil {
+		log.Fatalf("Failed to create RabbitMQ connection: %v", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connection was successful to RabbitMQ managment console")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("RabbitMQ connection closed.")
 }
